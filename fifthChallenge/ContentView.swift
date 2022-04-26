6c58dac7b6902c67514fe5576e3287ca16d6cf19
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-26 11:51:58 -0300

    Initial Commit

---
+++ fifthChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fifthChallenge
+//
+//  Created by Bruno Nascimento on 26/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fifthChallenge/fifthChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifthChallengeApp.swift
+//  fifthChallenge
+//
+//  Created by Bruno Nascimento on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct fifthChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
