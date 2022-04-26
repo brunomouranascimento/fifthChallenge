86ef82dc88af28d80a67fed4d81b80fc69484688
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-26 14:28:35 -0300

    feat: initial commit

--- fifthChallenge/ContentView.swift
+++ fifthChallenge/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    var states = [
+        "DF": "Distrito Federal",
+        "SP": "São Paulo",
+        "SC": "Santa Catarina",
+        "MG": "Minas Gerais",
+        "RJ": "Rio de Janeiro",
+        "ES": "Espírito Santo",
+        "PR": "Paraná",
+        "RS": "Rio Grande do Sul",
+    ]
+
+    var stateAndCountry: (String, String) = ("Estados", "Brasil")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            List {
+                Section("\(stateAndCountry.0) do \(stateAndCountry.1)") {
+                    ForEach(states.sorted(by: <), id: \.key) { uf, state in
+                        Text("\(state)")
+                            .onDisappear(perform: {
+                                print("\(uf) desapareceu da tela")
+                            })
+                    }
+                }
+            }
+            .onAppear(perform: {
+                print("--------Estados--------")
+                for state in states {
+                    print("\(state.key) - \(state.value)")
+                }
+                print("-----------------------")
+            })
+        }
     }
 }
 
